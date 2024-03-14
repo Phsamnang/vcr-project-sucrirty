a409899e2fe53d003bbca79379ee5436805e287d
Phsamnang <57305860+Phsamnang@users.noreply.github.com>
2024-03-14 13:00:55 +0700

    Update SecurityConfiguration.java

--- src/main/java/com/kosign/vcrprojectsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/kosign/vcrprojectsecurity/config/SecurityConfiguration.java
@@ -30,7 +30,6 @@ public class SecurityConfiguration {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedOriginPattern("*");
         corsConfiguration.setAllowedMethods(Arrays.asList(CorsConfiguration.ALL));
